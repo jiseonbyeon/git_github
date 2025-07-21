74ffbc3f9871f4e0853d1491b9f3b826531969c6
jiseonbyeon@gmail.com <jiseonbyeon@gmail.com>
2025-07-21 12:49:52 +0900

    version5

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -1,3 +1,9 @@
 
 # git add . 은 모든변경(추가)사항을 staging area 로 이동
 git add .
+
+# git commit은 staring area의 변경사항을 확정짓고 commitID생성
+git commit -m "메시지"
+
+# 원격 repo에 main 브랜치 변경사항(commitID)을 업로드
+git push origin main
